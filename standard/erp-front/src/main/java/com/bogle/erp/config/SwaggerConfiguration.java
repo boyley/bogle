49b8f45f1222c7f5be606741cf7ad875252825f0
Boyley <274163579@qq.com>
2015-09-25 18:01:33 +0800

    添加swagger

--- standard/erp-front/src/main/java/com/bogle/erp/config/SwaggerConfiguration.java
+++ standard/erp-front/src/main/java/com/bogle/erp/config/SwaggerConfiguration.java
@@ -4,18 +4,11 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.bind.RelaxedPropertyResolver;
 import org.springframework.context.EnvironmentAware;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.env.Environment;
-import org.springframework.http.ResponseEntity;
-import org.springframework.util.StopWatch;
 import springfox.documentation.builders.ApiInfoBuilder;
 import springfox.documentation.service.ApiInfo;
-import springfox.documentation.spi.DocumentationType;
-import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
-
-import static springfox.documentation.builders.PathSelectors.regex;
 
 
 /**
@@ -35,7 +28,7 @@ public class SwaggerConfiguration implements EnvironmentAware {
     }
 
 
-    private ApiInfo apiInfo() {
+    public ApiInfo apiInfo() {
         return new ApiInfoBuilder()
                 .title(propertyResolver.getProperty("title"))
                 .description(propertyResolver.getProperty("description"))
