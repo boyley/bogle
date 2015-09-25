d441db85eca8ad9c1e4e78c4421563f216da2ec8
Boyley <274163579@qq.com>
2015-09-25 14:43:20 +0800

    终于可以加载进入首页了，

--- standard/erp-front/src/main/webapp/assets/js/ace/ace.js
+++ standard/erp-front/src/main/webapp/assets/js/ace/ace.js
@@ -111,11 +111,11 @@ jQuery(function($) {
 				var path = document.location.pathname;
 
 				//for example in Ace HTML demo version we convert /ajax/index.html#page/gallery to > /ajax/content/gallery.html and load it
-				if(path.match(/(\/ajax\/)(index\.html)?/))
-					return path.replace(/(\/ajax\/)(index\.html)?/, '/ajax/content/'+hash.replace(/^page\//, '')+'.html') ;
-
+				//if(path.match(/(\/ajax\/)(index\.html)?/))
+				//	return path.replace(/(\/ajax\/)(index\.html)?/, hash.replace(/^page\//, '')+'.html') ;
+					return path + hash.replace(/\.html/,'') + ".html";
 				//for example in Ace PHP demo version we convert "ajax.php#page/dashboard" to "ajax.php?page=dashboard" and load it
-				return path + "?" + hash.replace(/\//, "=");
+				//return path + "?" + hash.replace(/\//, "=");
 			  }			  
 		}
 		   
