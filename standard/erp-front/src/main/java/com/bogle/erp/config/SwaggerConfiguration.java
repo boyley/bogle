f1a778a874c8730a3718c53750d38a852a07226c
Boyley <274163579@qq.com>
2015-09-25 17:29:13 +0800

    添加swagger

---
+++ standard/erp-front/src/main/java/com/bogle/erp/config/SwaggerConfiguration.java
@@ -0,0 +1,70 @@
+package com.bogle.erp.config;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.bind.RelaxedPropertyResolver;
+import org.springframework.context.EnvironmentAware;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.env.Environment;
+import org.springframework.http.ResponseEntity;
+import org.springframework.util.StopWatch;
+import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+import static springfox.documentation.builders.PathSelectors.regex;
+
+
+/**
+ * Created by Administrator on 2015/9/25.
+ */
+@Configuration
+@EnableSwagger2
+public class SwaggerConfiguration implements EnvironmentAware {
+    private final Logger log = LoggerFactory.getLogger(SwaggerConfiguration.class);
+
+    private RelaxedPropertyResolver propertyResolver;
+    public static final String DEFAULT_INCLUDE_PATTERN = "/api/.*";
+
+    @Override
+    public void setEnvironment(Environment environment) {
+        this.propertyResolver = new RelaxedPropertyResolver(environment, "swagger.");
+    }
+
+
+//    @Bean
+    public Docket swaggerSpringfoxDocket() {
+        log.debug("Starting Swagger");
+        StopWatch watch = new StopWatch();
+        watch.start();
+        Docket docket = new Docket(DocumentationType.SWAGGER_2)
+                .groupName("api")
+                .apiInfo(apiInfo())
+                .genericModelSubstitutes(ResponseEntity.class)
+                .forCodeGeneration(true)
+                .genericModelSubstitutes(ResponseEntity.class)
+                .select()
+                .paths(regex(DEFAULT_INCLUDE_PATTERN))
+                .build();
+        watch.stop();
+        log.debug("Started Swagger in {} ms", watch.getTotalTimeMillis());
+        return docket;
+    }
+
+
+    private ApiInfo apiInfo() {
+        return new ApiInfoBuilder()
+                .title(propertyResolver.getProperty("title"))
+                .description(propertyResolver.getProperty("description"))
+                .termsOfServiceUrl(propertyResolver.getProperty("termsOfServiceUrl"))
+                .contact(propertyResolver.getProperty("contact"))
+                .license(propertyResolver.getProperty("license"))
+                .licenseUrl(propertyResolver.getProperty("licenseUrl"))
+                .version(propertyResolver.getProperty("version"))
+                .build();
+    }
+}
--- standard/erp-front/src/main/java/com/bogle/erp/config/WebConfigurer.java
+++ standard/erp-front/src/main/java/com/bogle/erp/config/WebConfigurer.java
@@ -6,12 +6,14 @@ import org.springframework.boot.context.embedded.ConfigurableEmbeddedServletCont
 import org.springframework.boot.context.embedded.EmbeddedServletContainerCustomizer;
 import org.springframework.boot.context.embedded.MimeMappings;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
 /**
  * Created by lenovo on 2015/9/19.
  */
 @Configuration
-public class WebConfigurer implements EmbeddedServletContainerCustomizer {
+public class WebConfigurer extends WebMvcConfigurerAdapter implements EmbeddedServletContainerCustomizer {
 
     private final Logger log = LoggerFactory.getLogger(WebConfigurer.class);
 
@@ -23,5 +25,13 @@ public class WebConfigurer implements EmbeddedServletContainerCustomizer {
         container.setMimeMappings(mappings);
     }
 
+//    @Override
+//    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+//        registry.addResourceHandler("swagger-ui.html")
+//                .addResourceLocations("classpath:/META-INF/resources/");
+//
+//        registry.addResourceHandler("/webjars/**")
+//                .addResourceLocations("classpath:/META-INF/resources/webjars/");
+//    }
 
 }
