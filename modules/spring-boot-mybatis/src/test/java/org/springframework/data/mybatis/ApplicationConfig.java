97391388117af1954c080a9b9d0ec04d43bca8f9
Boyley <274163579@qq.com>
2015-10-08 12:39:57 +0800

    mybatis 配置

--- modules/spring-boot-mybatis/src/test/java/org/springframework/data/mybatis/ApplicationConfig.java
+++ modules/spring-boot-mybatis/src/test/java/org/springframework/data/mybatis/ApplicationConfig.java
@@ -4,9 +4,9 @@ import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.mybatis.repository.config.EnableMyBatisRepositories;
 
-@Configuration
-@EnableMyBatisRepositories
-@EnableAutoConfiguration
+//@Configuration
+//@EnableMyBatisRepositories
+//@EnableAutoConfiguration
 public class ApplicationConfig {
 
 }
