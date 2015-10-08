becdc399c76df81e8070239a66735dd9521dceef
Boyley <274163579@qq.com>
2015-10-08 13:34:47 +0800

    mybatis 配置

--- modules/spring-boot-mybatis/src/main/java/org/springframework/data/mybatis/repository/support/MyBatisRepositoryFactory.java
+++ modules/spring-boot-mybatis/src/main/java/org/springframework/data/mybatis/repository/support/MyBatisRepositoryFactory.java
@@ -5,6 +5,7 @@ import org.springframework.data.domain.Persistable;
 import org.springframework.data.mybatis.repository.MyBatisRepository;
 import org.springframework.data.mybatis.repository.query.MyBatisQueryLookupStrategy;
 import org.springframework.data.repository.core.EntityInformation;
+import org.springframework.data.repository.core.RepositoryInformation;
 import org.springframework.data.repository.core.RepositoryMetadata;
 import org.springframework.data.repository.core.support.RepositoryFactorySupport;
 import org.springframework.data.repository.query.EvaluationContextProvider;
@@ -15,43 +16,46 @@ import org.springframework.util.Assert;
 import java.io.Serializable;
 
 public class MyBatisRepositoryFactory extends RepositoryFactorySupport {
-	
-	private final SqlSessionTemplate sessionTemplate;
-	private EntityInformation entityInformation = null;
-	
-	public MyBatisRepositoryFactory(SqlSessionTemplate sessionTemplate) {
-		super();
-		Assert.notNull(sessionTemplate, "SqlSessionTemplate must not be null!");		
-		this.sessionTemplate = sessionTemplate;			
-	}
 
-	@Override
-	public <T, ID extends Serializable> EntityInformation<T, ID> getEntityInformation(Class<T> domainClass) {
-		if (null != entityInformation) return entityInformation;
+    private final SqlSessionTemplate sessionTemplate;
+    private EntityInformation entityInformation = null;
 
-		if (Persistable.class.isAssignableFrom(domainClass)) {
-			entityInformation = new MybatisPersistableEntityInformation(domainClass);
-		} else {
-			entityInformation = new MybatisEntityInformation<T, ID>(domainClass);
-		}
-		return entityInformation;
-	}
-	@Override
-	protected Class<?> getRepositoryBaseClass(RepositoryMetadata repositoryMetadata) {
-		return MyBatisRepository.class;
-	}
+    public MyBatisRepositoryFactory(SqlSessionTemplate sessionTemplate) {
+        super();
+        Assert.notNull(sessionTemplate, "SqlSessionTemplate must not be null!");
+        this.sessionTemplate = sessionTemplate;
+    }
 
-	@SuppressWarnings("rawtypes")
-	@Override
-	protected Object getTargetRepository(RepositoryMetadata repositoryMetadata) {	
-		return new SimpleMyBatisRepository(sessionTemplate, repositoryMetadata.getRepositoryInterface().getCanonicalName());
-	}
+    @Override
+    public <T, ID extends Serializable> EntityInformation<T, ID> getEntityInformation(Class<T> domainClass) {
+        if (null != entityInformation) return entityInformation;
+
+        if (Persistable.class.isAssignableFrom(domainClass)) {
+            entityInformation = new MybatisPersistableEntityInformation(domainClass);
+        } else {
+            entityInformation = new MybatisEntityInformation<T, ID>(domainClass);
+        }
+        return entityInformation;
+    }
+
+    @Override
+    protected Class<?> getRepositoryBaseClass(RepositoryMetadata repositoryMetadata) {
+        return MyBatisRepository.class;
+    }
+
+    @SuppressWarnings("rawtypes")
+    @Override
+    protected Object getTargetRepository(RepositoryMetadata repositoryMetadata) {
+        RepositoryInformation repositoryInformation = super.getRepositoryInformation(repositoryMetadata, MybatisPersistableEntityInformation.class);
+//		return new SimpleMyBatisRepository(sessionTemplate, repositoryMetadata.getRepositoryInterface().getCanonicalName());
+        return new SimpleMyBatisRepository(sessionTemplate, repositoryInformation, repositoryMetadata.getRepositoryInterface().getCanonicalName());
+    }
 
 
-	@Override
-	protected QueryLookupStrategy getQueryLookupStrategy(Key key, EvaluationContextProvider evaluationContextProvider) {
-		return MyBatisQueryLookupStrategy.create(sessionTemplate, key);
-	}
+    @Override
+    protected QueryLookupStrategy getQueryLookupStrategy(Key key, EvaluationContextProvider evaluationContextProvider) {
+        return MyBatisQueryLookupStrategy.create(sessionTemplate, key);
+    }
 
 
 }
--- modules/spring-boot-mybatis/src/main/java/org/springframework/data/mybatis/repository/support/SimpleMyBatisRepository.java
+++ modules/spring-boot-mybatis/src/main/java/org/springframework/data/mybatis/repository/support/SimpleMyBatisRepository.java
@@ -2,6 +2,7 @@ package org.springframework.data.mybatis.repository.support;
 
 import org.mybatis.spring.SqlSessionTemplate;
 import org.springframework.data.mybatis.repository.MyBatisRepository;
+import org.springframework.data.repository.core.RepositoryInformation;
 import org.springframework.util.Assert;
 
 import java.io.Serializable;
@@ -10,49 +11,49 @@ import java.util.List;
 import java.util.Map;
 
 /**
- *
  * Specifies Default binding for findOne, findAll, exists, count methods
  * between org.springframework.data.mybatis.repository.MyBatisRepository and mybatis mapper
  */
 public class SimpleMyBatisRepository<T, ID extends Serializable> extends SqlSessionRepositorySupport implements MyBatisRepository<T, ID> {
 
-	private final SqlSessionTemplate sessionTemplate;
-	private final String mappedStatementId;
+    private final SqlSessionTemplate sessionTemplate;
+    private final RepositoryInformation repositoryInformation;
 
-	public SimpleMyBatisRepository(SqlSessionTemplate sessionTemplate, String mappedStatementNamespace) {
-		super(sessionTemplate);
-		Assert.notNull(sessionTemplate, "SqlSessionTemplate must not be null!");
-		Assert.notNull(mappedStatementNamespace, "mappedStatementNamespace must not be null!");
-		this.sessionTemplate = sessionTemplate;
-		this.mappedStatementId = mappedStatementNamespace + ".find";
-	}
-
-	@Override
-	public T findOne(ID id) {
-		Map<String, ID> params = new HashMap<>();
-		params.put("pk", id);
-		return sessionTemplate.selectOne(mappedStatementId, params);
-	}
-
-	@Override
-	public List<T> findAll() {
-		Map<String, ID> params = new HashMap<>();
-		return sessionTemplate.selectList(mappedStatementId, params);
-	}
-
-	@Override
-	public boolean exists(ID id) {
-		return findOne(id) != null;
-	}
-
-	@Override
-	public long count() {
-		return findAll().size();
-	}
+    public SimpleMyBatisRepository(SqlSessionTemplate sessionTemplate, RepositoryInformation repositoryInformation) {
+        super(sessionTemplate);
+        Assert.notNull(sessionTemplate, "SqlSessionTemplate must not be null!");
+        this.sessionTemplate = sessionTemplate;
+        this.repositoryInformation = repositoryInformation;
+    }
 
 
-	@Override
-	protected String getNamespace() {
-		return mappedStatementId;
-	}
+
+    @Override
+    public T findOne(ID id) {
+        Map<String, ID> params = new HashMap<>();
+        params.put("pk", id);
+        return selectOne("find", params);
+    }
+
+    @Override
+    public List<T> findAll() {
+        Map<String, ID> params = new HashMap<>();
+        return sessionTemplate.selectList("find", params);
+    }
+
+    @Override
+    public boolean exists(ID id) {
+        return findOne(id) != null;
+    }
+
+    @Override
+    public long count() {
+        return findAll().size();
+    }
+
+
+    @Override
+    protected String getNamespace() {
+        return this.repositoryInformation.getRepositoryInterface().getName();
+    }
 }
