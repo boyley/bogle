cfdcbe031720ab68da57b27ee1f0f234ad972f65
Boyley <274163579@qq.com>
2015-10-08 17:23:14 +0800

    mybatis 配置

--- modules/spring-boot-mybatis/src/main/java/org/springframework/data/mybatis/repository/MyBatisRepository.java
+++ modules/spring-boot-mybatis/src/main/java/org/springframework/data/mybatis/repository/MyBatisRepository.java
@@ -1,8 +1,5 @@
 package org.springframework.data.mybatis.repository;
 
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.Pageable;
-import org.springframework.data.domain.Sort;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.data.repository.NoRepositoryBean;
 
