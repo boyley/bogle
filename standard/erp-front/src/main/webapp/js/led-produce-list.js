a271574cc77e04fb60335023ae3f27fe2d8185a3
Boyley <274163579@qq.com>
2015-10-18 19:15:15 +0800

    修改dataTable，想办法改成异步

--- standard/erp-front/src/main/webapp/js/led-produce-list.js
+++ standard/erp-front/src/main/webapp/js/led-produce-list.js
@@ -7,8 +7,6 @@ var scripts = [
     "../../assets/js/jquery-ui.custom.js",
     "../../assets/js/jquery.ui.touch-punch.js",
     "../../assets/js/bootbox.js",
-    "../../assets/js/jquery.easypiechart.js",
-    "../../assets/js/jquery.gritter.js",
     "../../assets/js/spin.js",
     null]
 $('.page-content-area').ace_ajax('loadScripts', scripts, function () {
@@ -32,6 +30,31 @@ $('.page-content-area').ace_ajax('loadScripts', scripts, function () {
                 }
             });
         });
+
+        var language = {
+            "sProcessing": "处理中...",
+            "sLengthMenu": "每页显示 _MENU_ 条结果",
+            "sZeroRecords": "没有匹配结果",
+            "sInfo": "显示第 _START_ 至 _END_ 项结果，共 _TOTAL_ 项",
+            "sInfoEmpty": "显示第 0 至 0 项结果，共 0 项",
+            "sInfoFiltered": "(由 _MAX_ 项结果过滤)",
+            "sInfoPostFix": "",
+            "sSearch": "搜索:",
+            "sUrl": "",
+            "sEmptyTable": "表中数据为空",
+            "sLoadingRecords": "载入中...",
+            "sInfoThousands": ",",
+            "oPaginate": {
+                "sFirst": "首页",
+                "sPrevious": "上一页",
+                "sNext": "下一页",
+                "sLast": "末页"
+            },
+            "oAria": {
+                "sSortAscending": ": 以升序排列此列",
+                "sSortDescending": ": 以降序排列此列"
+            }
+        };
 
         $('#dynamic-table').dataTable({
             'language': language,
